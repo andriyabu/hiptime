bb82737deb3e5feb15b3dcd9406447d1f530c16b
ANDRI YABU NDAPA REHING <andriyabu@gmail.com>
2021-02-08 20:58:11 +0700

    add bootstrap and simple_form gem, create items model, controller and view and create CRUD function

---
+++ app/controllers/items_controller.rb
@@ -0,0 +1,62 @@
+class ItemsController < ApplicationController
+before_action :find_item, only: [:edit, :show, :update, :destroy]
+    def index
+        @items = Item.all.order(created_at: :desc)
+    end
+
+    def new
+        @item = Item.new
+    end
+
+    def create
+        @item = Item.new(item_params)
+        if @item.save
+          flash[:success] = "Object successfully created"
+          redirect_to @item
+        else
+          flash[:error] = "Something went wrong"
+          render 'new'
+        end
+    end
+
+    def edit
+        
+    end
+
+    def show
+        
+    end
+
+    def update
+        
+        if @item.update(item_params)
+          flash[:success] = "Object was successfully updated"
+          redirect_to @item
+        else
+          flash[:error] = "Something went wrong"
+          render 'edit'
+        end
+    end
+
+    def destroy
+        
+        if @item.destroy
+            flash[:success] = 'Object was successfully deleted.'
+            redirect_to items_path
+        else
+            flash[:error] = 'Something went wrong'
+            redirect_to @item
+        end
+    end
+    
+    private
+
+    def find_item
+        @item = Item.find(params[:id])
+    end
+
+    def item_params
+        params.require(:item).permit(:title,:description)
+    end    
+    
+end
